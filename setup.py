3f6ba679bccd7f2e825b34d2a7200e6f208d9edc
Sebastian Vetter <sebastian.vetter@tangentone.com.au>
2012-05-09 16:49:40 +1000

    added new version of package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-url-tracker",
-    version = '0.1.0',
+    version = '0.1.1',
     url = "https://github.com/tangentlabs/django-url-tracker",
     author = "Sebastian Vetter",
     author_email = "sebastian.vetter@tangentone.com.au",
